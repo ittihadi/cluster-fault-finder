a4c7e38dda4fd96c93cee073e799cca5893aa2bf
Hadi <rittihadi@gmail.com>
2026-04-26 23:37:09 +0700

    implement basic visualizer

--- src/main.zig
+++ src/main.zig
@@ -23,8 +23,8 @@ const texture_frames = struct {
 const colors = struct {
     pub const hover: rl.Color = .init(0xff, 0xff, 0xff, 0xff);
     pub const safe: rl.Color = .init(0x3e, 0xfb, 0x3e, 0xff);
-    pub const suspect_1: rl.Color = .init(0xea, 0xf3, 0x12, 0xff);
-    pub const suspect_2: rl.Color = .init(0xf3, 0xaf, 0x12, 0xff);
+    pub const suspect_a: rl.Color = .init(0xea, 0xf3, 0x12, 0xff);
+    pub const suspect_b: rl.Color = .init(0xf3, 0xaf, 0x12, 0xff);
     pub const faulty: rl.Color = .init(0xf3, 0x2e, 0x2e, 0xff);
 };
 
@@ -44,11 +44,13 @@ var setup_mode = true;
 var ui_bounds: std.array_hash_map.String(rl.Rectangle) = .empty;
 var solve_steps: []solver.SolveStep = &.{};
 
-const visualization = struct {
+const visualizer = struct {
     pub var playing = false;
     pub var current_frame: u32 = 0;
     pub var next_step: u32 = 0;
-    pub var step_frame_bounds: []u32 = &.{};
+    /// Maps which frame a single step (single instance in a group) happens
+    pub var step_frame_mapping: []u32 = &.{};
+    pub var current_checks: u32 = 0;
 };
 
 pub fn main(init: std.process.Init) !void {
@@ -58,8 +60,6 @@ pub fn main(init: std.process.Init) !void {
 
     var prng: std.Random.DefaultPrng = .init(@intCast(std.Io.Timestamp.now(init.io, .real).toMilliseconds()));
     const rng = prng.random();
-
-    _ = rng.int(u32);
 
     rl.setConfigFlags(.{ .window_resizable = true });
     rl.initWindow(1280, 720, "Fake Coin Problem");
@@ -76,6 +76,7 @@ pub fn main(init: std.process.Init) !void {
     defer nodes.deinit(init.gpa);
     defer ui_bounds.deinit(init.gpa);
     defer init.gpa.free(solve_steps);
+    defer init.gpa.free(visualizer.step_frame_mapping);
 
     // Add UI element locations
     try ui_bounds.put(init.gpa, "add_many", .init(10, 10, 120, 30));
@@ -84,12 +85,13 @@ pub fn main(init: std.process.Init) !void {
     try ui_bounds.put(init.gpa, "node_count", .init(140, 55, 200, 20));
     try ui_bounds.put(init.gpa, "cant_solve", .init(10, 90, 330, 60));
 
-    try ui_bounds.put(init.gpa, "sim_back", .init(10, 10, 30, 30));
-    try ui_bounds.put(init.gpa, "sim_play_pause", .init(50, 10, 30, 30));
-    try ui_bounds.put(init.gpa, "sim_forward", .init(90, 10, 30, 30));
-    try ui_bounds.put(init.gpa, "sim_node_count", .init(10, 50, 240, 30));
-    try ui_bounds.put(init.gpa, "sim_step", .init(130, 90, 240, 30));
-    try ui_bounds.put(init.gpa, "sim_frame", .init(130, 130, 240, 30));
+    try ui_bounds.put(init.gpa, "sim_exit", .init(10, 10, 30, 30));
+    try ui_bounds.put(init.gpa, "sim_back", .init(50, 10, 30, 30));
+    try ui_bounds.put(init.gpa, "sim_play_pause", .init(90, 10, 30, 30));
+    try ui_bounds.put(init.gpa, "sim_forward", .init(130, 10, 30, 30));
+    try ui_bounds.put(init.gpa, "sim_node_count", .init(10, 50, 240, 20));
+    try ui_bounds.put(init.gpa, "sim_check", .init(10, 80, 240, 20));
+    try ui_bounds.put(init.gpa, "sim_frame", .init(10, 110, 240, 20));
 
     texture = blk: {
         const img = try rl.loadImageFromMemory(".png", server_tex_file);
@@ -178,6 +180,32 @@ pub fn main(init: std.process.Init) !void {
             }
         }
 
+        // Update visualizer
+        blk: {
+            if (solve_steps.len == 0) {
+                break :blk;
+            }
+
+            if (visualizer.playing) {
+                visualizer.current_frame += 1;
+            }
+
+            var frame_bound = visualizer.step_frame_mapping[visualizer.next_step];
+            while (frame_bound <= visualizer.current_frame) {
+                applyStep(visualizer.next_step);
+                switch (solve_steps[visualizer.next_step].step) {
+                    .found_at_index => {
+                        visualizer.playing = false;
+                        break;
+                    },
+                    else => {
+                        visualizer.next_step += 1;
+                        frame_bound = visualizer.step_frame_mapping[visualizer.next_step];
+                    },
+                }
+            }
+        }
+
         // Draw
         rl.beginDrawing();
         defer rl.endDrawing();
@@ -232,8 +260,8 @@ pub fn main(init: std.process.Init) !void {
                         switch (node.state) {
                             .counterfeit => colors.faulty,
                             .safe => colors.safe,
-                            .suspect_a => colors.suspect_1,
-                            .suspect_b => colors.suspect_2,
+                            .suspect_a => colors.suspect_a,
+                            .suspect_b => colors.suspect_b,
                             else => unreachable,
                         },
                     );
@@ -241,7 +269,7 @@ pub fn main(init: std.process.Init) !void {
             }
 
             // Preview placement
-            if (selected_idx == null and !hovering_ui) {
+            if (selected_idx == null and !hovering_ui and setup_mode) {
                 const dest_rect: rl.Rectangle = .init(mouse_pos_world.x, mouse_pos_world.y, 32, 32);
                 rl.drawTexturePro(texture, texture_frames.base, dest_rect, texture_orig, camera.rotation, .init(0xff, 0xff, 0xff, 0x55));
             }
@@ -308,23 +336,30 @@ pub fn main(init: std.process.Init) !void {
                 nodes.sortByX(0, nodes.count());
                 solve_steps = try solver.solve(&nodes, init.gpa, 0, nodes.count(), 0);
 
-                // for (solve_steps) |step| {
-                //     switch (step.step) {
-                //         .change_state => |state_change| {
-                //             std.debug.print(
-                //                 "Step: {d}, change node {d} state from {s} to {s}\n",
-                //                 .{ step.id, state_change.index, @tagName(state_change.from), @tagName(state_change.to) },
-                //             );
-                //             // for the time being
-                //             nodes.array_list.items[state_change.index].state = state_change.to;
-                //         },
-                //         .found_at_index => |idx| {
-                //             std.debug.print("Step: {d} found faulty node at: {d}\n", .{ step.id, idx });
-                //             nodes.array_list.items[idx].state = .counterfeit;
-                //         },
-                //         else => unreachable,
-                //     }
-                // }
+                if (builtin.mode == .Debug) {
+                    for (solve_steps) |step| {
+                        switch (step.step) {
+                            .change_state => |state_change| {
+                                std.debug.print(
+                                    "Step: {d}, change node {d} state from {s} to {s}\n",
+                                    .{ step.id, state_change.index, @tagName(state_change.from), @tagName(state_change.to) },
+                                );
+                            },
+                            .found_at_index => |idx| {
+                                std.debug.print("Step: {d}, found faulty node at: {d}\n", .{ step.id, idx });
+                            },
+                            .compare_size => |size| {
+                                std.debug.print("Step: {d}, current comparison size: {d}\n", .{ step.id, size });
+                            },
+                            .start_compare => {
+                                std.debug.print("Step: {d}, start comparison\n", .{step.id});
+                            },
+                            else => unreachable,
+                        }
+                    }
+                }
+
+                try setupVisualizer(init.gpa, rng);
                 setup_mode = false;
             }
 
@@ -337,28 +372,45 @@ pub fn main(init: std.process.Init) !void {
         }
         // Simulation/Visualization UI
         else {
+            const sim_exit = ui_bounds.get("sim_exit").?;
+            if (rg.button(sim_exit, rg.iconText(@intFromEnum(rg.IconName.cross), ""))) {
+                visualizer.playing = false;
+                setup_mode = true;
+            }
+
             const sim_back = ui_bounds.get("sim_back").?;
             if (rg.button(sim_back, rg.iconText(@intFromEnum(rg.IconName.player_previous), ""))) {
-                //
+                if (visualizer.playing) {
+                    visualizer.playing = false;
+                }
+                // Unapply current step
             }
 
             const sim_toggle = ui_bounds.get("sim_play_pause").?;
-            const icon = if (visualization.playing) @intFromEnum(rg.IconName.player_pause) else @intFromEnum(rg.IconName.player_play);
+            const icon = if (visualizer.playing) @intFromEnum(rg.IconName.player_pause) else @intFromEnum(rg.IconName.player_play);
             if (rg.button(sim_toggle, rg.iconText(icon, ""))) {
-                //
+                visualizer.playing = !visualizer.playing;
             }
 
             const sim_forward = ui_bounds.get("sim_forward").?;
             if (rg.button(sim_forward, rg.iconText(@intFromEnum(rg.IconName.player_next), ""))) {
-                //
+                if (visualizer.playing) {
+                    visualizer.playing = false;
+                }
+                // Go to next step
             }
 
             const sim_node_count = ui_bounds.get("sim_node_count").?;
-            _ = sim_node_count;
-            const sim_step = ui_bounds.get("sim_step").?;
-            _ = sim_step;
+            const node_count_text = try std.fmt.allocPrintSentinel(frame_alloc, "Node count: {d}", .{nodes.count()}, 0);
+            _ = rg.label(sim_node_count, node_count_text);
+
+            const sim_check = ui_bounds.get("sim_check").?;
+            const node_check_text = try std.fmt.allocPrintSentinel(frame_alloc, "Current Checks: {d}", .{visualizer.current_checks}, 0);
+            _ = rg.label(sim_check, node_check_text);
+
             const sim_frame = ui_bounds.get("sim_frame").?;
-            _ = sim_frame;
+            const node_frame_text = try std.fmt.allocPrintSentinel(frame_alloc, "Current Frame: {d}", .{visualizer.current_frame}, 0);
+            _ = rg.label(sim_frame, node_frame_text);
         }
 
         _ = frame_arena.reset(.retain_capacity);
@@ -372,4 +424,76 @@ fn hasFaultyNode() bool {
         }
     }
     return false;
+}
+
+fn setupVisualizer(gpa: std.mem.Allocator, rng: std.Random) !void {
+    visualizer.playing = false;
+    visualizer.next_step = 0;
+    visualizer.current_frame = 0;
+    visualizer.current_checks = 0;
+
+    gpa.free(visualizer.step_frame_mapping);
+    visualizer.step_frame_mapping = try gpa.alloc(u32, solve_steps.len);
+
+    // Setup step -> frame mapping
+    const startup_padding: u32 = 30;
+    var current_frame = startup_padding;
+    var current_step: usize = 0;
+    var current_comparison_size: usize = 0;
+    const fast_comparison_thresh = 30;
+
+    while (current_step < solve_steps.len) : (current_step += 1) {
+        const curr = solve_steps[current_step];
+        const next = if (current_step < solve_steps.len - 1) solve_steps[current_step + 1] else curr;
+        visualizer.step_frame_mapping[current_step] = current_frame;
+        switch (curr.step) {
+            .start_compare => {
+                current_frame += rng.intRangeAtMost(u32, 120, 180);
+            },
+            .compare_size => |size| {
+                current_comparison_size = size;
+            },
+            .change_state => |change| {
+                switch (change.to) {
+                    .safe, .neutral, .counterfeit => {
+                        if (next.id > curr.id) {
+                            current_frame += 20;
+                        }
+                    },
+                    .suspect_a, .suspect_b => {
+                        if (current_comparison_size >= fast_comparison_thresh) {
+                            current_frame += 1;
+                        } else {
+                            current_frame += 6;
+                        }
+                    },
+                }
+            },
+            .found_at_index => {},
+            .incorrect_input => unreachable,
+        }
+    }
+}
+
+fn applyStep(step: usize) void {
+    switch (solve_steps[step].step) {
+        .compare_size, .found_at_index => {},
+        .start_compare => visualizer.current_checks += 1,
+        .change_state => |change| {
+            nodes.array_list.items[change.index].state = change.to;
+            // std.debug.print("Changed state of {d} from {s} to {s}\n", .{ change.index, @tagName(change.from), @tagName(change.to) });
+        },
+        .incorrect_input => unreachable,
+    }
+}
+
+fn unapplyStep(step: usize) void {
+    switch (solve_steps[step].step) {
+        .compare_size, .found_at_index => {},
+        .start_compare => visualizer.current_checks -= 1,
+        .change_state => |change| {
+            nodes.array_list.items[change.index].state = change.from;
+        },
+        .incorrect_input => unreachable,
+    }
 }
