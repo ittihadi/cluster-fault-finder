150e256c54d2b3829c7d5993cf6893d07064c5f2
Hadi <rittihadi@gmail.com>
2026-04-27 15:39:42 +0700

    add in welcome and help screen and a help button

--- src/main.zig
+++ src/main.zig
@@ -54,13 +54,6 @@ const visualizer = struct {
 };
 
 pub fn main(init: std.process.Init) !void {
-    var frame_arena: std.heap.ArenaAllocator = .init(init.gpa);
-    defer frame_arena.deinit();
-    const frame_alloc = frame_arena.allocator();
-
-    var prng: std.Random.DefaultPrng = .init(@intCast(std.Io.Timestamp.now(init.io, .real).toMilliseconds()));
-    const rng = prng.random();
-
     rl.setConfigFlags(.{ .window_resizable = true });
     rl.initWindow(1280, 720, "Fake Coin Problem");
     defer rl.closeWindow();
@@ -72,6 +65,13 @@ pub fn main(init: std.process.Init) !void {
 
     camera.offset.x = @as(f32, @floatFromInt(rl.getScreenWidth())) / 2;
     camera.offset.y = @as(f32, @floatFromInt(rl.getScreenHeight())) / 2;
+
+    var frame_arena: std.heap.ArenaAllocator = .init(init.gpa);
+    defer frame_arena.deinit();
+    const frame_alloc = frame_arena.allocator();
+
+    var prng: std.Random.DefaultPrng = .init(@intCast(std.Io.Timestamp.now(init.io, .real).toMilliseconds()));
+    const rng = prng.random();
 
     defer nodes.deinit(init.gpa);
     defer ui_bounds.deinit(init.gpa);
@@ -92,6 +92,8 @@ pub fn main(init: std.process.Init) !void {
     try ui_bounds.put(init.gpa, "sim_node_count", .init(10, 50, 240, 20));
     try ui_bounds.put(init.gpa, "sim_check", .init(10, 80, 240, 20));
     try ui_bounds.put(init.gpa, "sim_frame", .init(10, 110, 240, 20));
+
+    try ui_bounds.put(init.gpa, "info", .init(@floatFromInt(rl.getScreenWidth() - 40), 10, 30, 30));
 
     texture = blk: {
         const img = try rl.loadImageFromMemory(".png", server_tex_file);
@@ -119,17 +121,38 @@ pub fn main(init: std.process.Init) !void {
             @branchHint(.unlikely);
             camera.offset.x = @as(f32, @floatFromInt(rl.getScreenWidth())) / 2;
             camera.offset.y = @as(f32, @floatFromInt(rl.getScreenHeight())) / 2;
+
+            try ui_bounds.put(init.gpa, "info", .init(@floatFromInt(rl.getScreenWidth() - 40), 10, 30, 30));
         }
 
         var hovering_ui = false;
-        for (ui_bounds.values()) |bound| {
+        for (ui_bounds.keys(), ui_bounds.values()) |key, bound| {
+            if ((setup_mode and std.mem.startsWith(u8, key, "sim")) or
+                (!setup_mode and !std.mem.startsWith(u8, key, "sim")))
+            {
+                continue;
+            }
+
             if (rl.checkCollisionPointRec(rl.getMousePosition(), bound)) {
                 hovering_ui = true;
                 break;
             }
         }
 
-        if (!hovering_ui) {
+        if (show_welcome_screen) {
+            if (rl.isKeyPressed(.space) or rl.isMouseButtonPressed(.left) or
+                rl.isMouseButtonPressed(.middle) or rl.isMouseButtonPressed(.right))
+            {
+                show_welcome_screen = false;
+            }
+        } else if (show_help_screen) {
+            if ((rl.isKeyPressed(.space) or rl.isMouseButtonPressed(.left) or
+                rl.isMouseButtonPressed(.middle) or rl.isMouseButtonPressed(.right)) and
+                !rl.checkCollisionPointRec(rl.getMousePosition(), ui_bounds.get("info").?))
+            {
+                show_help_screen = false;
+            }
+        } else if (!hovering_ui) {
             // Check for camera drag
             if (rl.isKeyPressed(.space) or rl.isMouseButtonPressed(.middle)) {
                 drag_mode = true;
@@ -181,12 +204,12 @@ pub fn main(init: std.process.Init) !void {
         }
 
         // Update visualizer
-        blk: {
+        if (!setup_mode) blk: {
             if (solve_steps.len == 0) {
                 break :blk;
             }
 
-            if (visualizer.playing) {
+            if (visualizer.playing and !show_help_screen) {
                 visualizer.current_frame += 1;
 
                 var frame_bound = visualizer.step_frame_mapping[visualizer.next_step];
@@ -284,7 +307,7 @@ pub fn main(init: std.process.Init) !void {
             }
 
             // Preview placement
-            if (selected_idx == null and !hovering_ui and setup_mode) {
+            if (selected_idx == null and !hovering_ui and setup_mode and !show_help_screen and !show_welcome_screen) {
                 const dest_rect: rl.Rectangle = .init(mouse_pos_world.x, mouse_pos_world.y, 32, 32);
                 rl.drawTexturePro(texture, texture_frames.base, dest_rect, texture_orig, camera.rotation, .init(0xff, 0xff, 0xff, 0x55));
             }
@@ -304,7 +327,8 @@ pub fn main(init: std.process.Init) !void {
         // Draw UI
 
         // Setup UI
-        if (setup_mode) {
+        // "I don't want big diffs which is why I didn't just indent another level lol"
+        if (setup_mode and !show_welcome_screen and !show_help_screen) {
             if (rg.button(ui_bounds.get("add_many").?, "Add 50")) {
                 const to_place = 50;
                 var placed: u32 = 0;
@@ -341,7 +365,7 @@ pub fn main(init: std.process.Init) !void {
             if (!can_solve) {
                 const prev = rg.getStyle(.default, .{ .control = .text_color_normal });
                 rg.setStyle(.default, .{ .control = .text_color_normal }, colors.faulty.toInt());
-                _ = rg.label(ui_bounds.get("cant_solve").?, "Please ensure at least two nodes\nexist and that one is marked\nfaulty");
+                _ = rg.label(ui_bounds.get("cant_solve").?, "Ensure at least two nodes exist\nand that one is marked faulty");
                 rg.setStyle(.default, .{ .control = .text_color_normal }, prev);
                 rg.disable();
             }
@@ -386,7 +410,7 @@ pub fn main(init: std.process.Init) !void {
             _ = rg.label(ui_bounds.get("node_count").?, node_count_text);
         }
         // Simulation/Visualization UI
-        else {
+        else if (!setup_mode and !show_welcome_screen and !show_help_screen) {
             const sim_exit = ui_bounds.get("sim_exit").?;
             if (rg.button(sim_exit, rg.iconText(@intFromEnum(rg.IconName.cross), ""))) {
                 visualizer.playing = false;
@@ -453,6 +477,83 @@ pub fn main(init: std.process.Init) !void {
             const sim_frame = ui_bounds.get("sim_frame").?;
             const node_frame_text = try std.fmt.allocPrintSentinel(frame_alloc, "Current Frame: {d}", .{visualizer.current_frame}, 0);
             _ = rg.label(sim_frame, node_frame_text);
+        }
+
+        if (show_help_screen) {
+            const center_x = @as(f32, @floatFromInt(rl.getScreenWidth())) / 2;
+            const center_y = @as(f32, @floatFromInt(rl.getScreenHeight())) / 2;
+            const panel_bounds: rl.Rectangle = if (setup_mode)
+                .init(center_x - 250, center_y - 110, 500, 220)
+            else
+                .init(center_x - 250, center_y - 170, 500, 340);
+            const panel_color = rg.getStyle(.default, .{ .default = .background_color });
+            const text_color: rl.Color = .fromInt(@bitCast(rg.getStyle(.default, .{ .control = .text_color_normal })));
+
+            rl.drawRectangle(0, 0, rl.getScreenWidth(), rl.getScreenHeight(), .init(0x88, 0x88, 0x88, 0x44));
+            rl.drawRectangleRounded(panel_bounds, 0.05, 10, .fromInt(@bitCast(panel_color)));
+
+            const title_text: [:0]const u8 = "Help";
+            const title_w: f32 = @floatFromInt(rl.measureText(title_text, 20));
+            rl.drawText(title_text, @trunc(center_x - title_w / 2), @trunc(panel_bounds.y + 16), 20, text_color);
+
+            const inst_txt: [:0]const u8 = if (setup_mode)
+                \\- Place nodes by Left clicking
+                \\- Remove nodes by Right clicking
+                \\- Move by holding Middle Mouse click or
+                \\  Space and dragging the screen
+                \\- Left click a node to mark it as faulty
+            else
+                \\- Use the Close button to go back to setup
+                \\  mode
+                \\- Use the Back, Pause/Play, and Forward
+                \\  buttons to navigate around the simulation
+                \\- Move by holding Middle Mouse Button or
+                \\  Space and dragging the screen
+                \\Nodes are outlined the following way:
+                \\- Orange / Yellow - Groups being compared
+                \\- Red - Found faulty node
+                \\- Green - Known safe node
+                \\- No outline - Not currently considered
+            ;
+            rl.drawText(inst_txt, @trunc(panel_bounds.x + 16), @trunc(panel_bounds.y + 52), 20, text_color);
+
+            const author_txt: [:0]const u8 = "Application by Ittihadi Ramadhan (24343038)";
+            const author_w: f32 = @floatFromInt(rl.measureText(author_txt, 10));
+            const author_y: i32 = @trunc(panel_bounds.y + panel_bounds.height - 16 - 10);
+            rl.drawText(author_txt, @trunc(center_x - author_w / 2), author_y, 10, text_color);
+        }
+
+        if (show_welcome_screen) {
+            // Draw welcome screen
+            const center_x = @as(f32, @floatFromInt(rl.getScreenWidth())) / 2;
+            const center_y = @as(f32, @floatFromInt(rl.getScreenHeight())) / 2;
+            const panel_bounds: rl.Rectangle = .init(center_x - 250, center_y - 100, 500, 200);
+            const panel_color = rg.getStyle(.default, .{ .default = .background_color });
+            const text_color: rl.Color = .fromInt(@bitCast(rg.getStyle(.default, .{ .control = .text_color_normal })));
+
+            rl.drawRectangle(0, 0, rl.getScreenWidth(), rl.getScreenHeight(), .init(0x88, 0x88, 0x88, 0x44));
+            rl.drawRectangleRounded(panel_bounds, 0.05, 10, rl.colorAlpha(.fromInt(@bitCast(panel_color)), 0.7));
+
+            const title_text: [:0]const u8 = "Cluster Computer Fault Tracker Simulator";
+            const title_w: f32 = @floatFromInt(rl.measureText(title_text, 20));
+            rl.drawText(title_text, @trunc(center_x - title_w / 2), @trunc(panel_bounds.y + 16), 20, text_color);
+
+            const inst_txt: [:0]const u8 =
+                \\- Setup a cluster nodes with left click
+                \\- Simulate a node being faulty
+                \\- Press the Find Faulty button to find the
+                \\  faulty node with decrease and conquer
+            ;
+            rl.drawText(inst_txt, @trunc(panel_bounds.x + 16), @trunc(panel_bounds.y + 52), 20, text_color);
+
+            const anywhere_txt: [:0]const u8 = "Click anywhere to start";
+            const anywhere_w: f32 = @floatFromInt(rl.measureText(anywhere_txt, 10));
+            const anywhere_y: i32 = @trunc(panel_bounds.y + panel_bounds.height - 16 - 10);
+            rl.drawText(anywhere_txt, @trunc(center_x - anywhere_w / 2), anywhere_y, 10, text_color);
+        } else {
+            if (rg.button(ui_bounds.get("info").?, rg.iconText(@intFromEnum(rg.IconName.help), ""))) {
+                show_help_screen = !show_help_screen;
+            }
         }
 
         _ = frame_arena.reset(.retain_capacity);
